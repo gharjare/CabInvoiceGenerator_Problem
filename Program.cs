7a12d550cbef842f064f943e88496966d1edb076
gharjare <gharjarechetan@gmail.com>
2022-11-16 20:28:24 +0530

    Add file 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CabInvoiceGenerator
+{
+    class program
+    {
+        enum Month
+        {
+            January,
+            February,
+            March,
+        }
+        public static void Main(string[] args)
+        {
+            int val = (int)Month.February;
+            Console.WriteLine(val);
+        }
+    }
+}
