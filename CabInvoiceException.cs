52d8561c07052c83100b27a57a9c4fb9747257c2
gharjare <gharjarechetan@gmail.com>
2022-11-16 20:34:39 +0530

    Add exception

---
+++ CabInvoiceException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class CabInvoiceException:Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_RIDE_TYPE,
+            INVALID_DISTANCE,
+            INVALID_TIME,
+            INVALID_USER_ID,
+            Null_Rides
+        }
+
+        ExceptionType type;
+
+        public CabInvoiceException(ExceptionType type, string message):base(message)
+        {
+            this.type = type;
+        }
+    }
+}
